77465daf823cd5240f1064f341f060a2422b01f3
Marshall <marshalldieme@gmail.com>
2021-12-14 08:35:42 +0000

    Router Dom integration

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import './navbar.css'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <header>
+                Navbar
+            </header>
+        );
+    }
+}
+
+export default Navbar;
