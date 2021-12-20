95589e3e172db09036dbbc49bf730cf262fcc2d1
Marshall <marshalldieme@gmail.com>
2021-12-20 08:18:30 +0000

    Finalization

--- src/components/pricing/Pricing.js
+++ src/components/pricing/Pricing.js
@@ -8,7 +8,7 @@ class Pricing extends Component {
                 <div className="price-card">
                     <span className='card-banner'></span>
                     <div className="card-content">
-                        <h4>Registration</h4>
+                        <h4>Saraya Bootcamp</h4>
                         <ul>
                             <li><span><ion-icon name="checkmark-done-outline"></ion-icon></span> 6 months of training</li>
                             <li><span><ion-icon name="checkmark-done-outline"></ion-icon></span> 2 to 6 months of internship</li>
@@ -16,6 +16,19 @@ class Pricing extends Component {
                         </ul>
                         <div className="price">
                             <h5>CFA 250,000</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="price-card">
+                    <div className="card-content">
+                        <h4>Dakar Tech University</h4>
+                        <ul>
+                            <li><span><ion-icon name="checkmark-done-outline"></ion-icon></span> Bachelor</li>
+                            <li><span><ion-icon name="checkmark-done-outline"></ion-icon></span> Master</li>
+                            <li><span><ion-icon name="checkmark-done-outline"></ion-icon></span> PhD</li>
+                        </ul>
+                        <div className="price">
+                            <h5>Read more</h5>
                         </div>
                     </div>
                 </div>
